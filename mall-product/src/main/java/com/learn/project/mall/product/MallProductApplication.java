3f40f9d75fbb9c7206fe4bca8674abeb74e3542b
chenfuyuan <chenfuyuan0713@163.com>
2021-04-16 15:18:34 +0800

    fix:修复代码生成器生成代码报错。删除import org.apache.shiro.authz.annotation.RequiresPermissions;

--- mall-product/src/main/java/com/learn/project/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/learn/project/mall/product/MallProductApplication.java
@@ -1,9 +1,22 @@
 package com.learn.project.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 整合Mybatis-plus
+ * 1. 导入依赖
+ * 2. 配置
+ *     1)。配置数据源
+ *      1.1). 导入数据库驱动
+ *            引入依赖 mysql-connector-java
+ *      1.2). 在application.yml里面配置数据源信息
+ *     2). 配置Mybatis-plus
+ *      2.1). 使用MapperScaner
+ */
 @SpringBootApplication
+@MapperScan("com.learn.project.mall.product.dao")
 public class MallProductApplication {
 
     public static void main(String[] args) {
